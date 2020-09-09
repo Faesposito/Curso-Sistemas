f2a287ca4ef5e0e72f300105a34d84b7baf02458
Faesposito <faespo@live.com.ar>
2020-09-09 16:19:04 -0300

    modificacions Crud

--- Tema 7 - Desarrollo de Aplicaciones a travez de API's/CRUM-ABM/view.js
+++ Tema 7 - Desarrollo de Aplicaciones a travez de API's/CRUM-ABM/view.js
@@ -18,40 +18,21 @@
 */
 
 import {
-	UserModel
-} from './model.js';
+  UserModel
+} from "./model.js";
 
 class UserView {
+  constructor(id) {
+    this.innerModel = new UserModel();
+    this.id = id;
 
-	constructor(id) {
+    this.update();
+  }
 
-		this.innerModel = new UserModel();
-		this.id = id;
-
-		this.update();
-	}
-
-	/* 	buttonPres() {
-
-			var buttons = document.getElementsByTagName("button");
-			var buttonsCount = buttons.length;
-			let id;
-
-			for (var i = 0; i <= buttonsCount; i += 1) {
-				buttons[i].click = function (e) {
-					id = this.id;
-
-				};
-			}
-
-			return id;
-		} */
-
-	update() {
-
-		let html =
-			`<h1>CRUD Users</h1>
-		<table id="CRUD-Tabble">
+  update() {
+    let html = `<h1>CRUD Users</h1>
+    <div >
+		<table id="CRUD-Wrapper">
 		<tr>
 			<th>Username</th>
 			<th>Nombre de usuario</th>
@@ -59,95 +40,88 @@ class UserView {
 			<th colspan = 2 >Acciones</th>
 		<tr>`;
 
-		for (let user of this.innerModel.getAll()) {
-			html +=
-				`<tr>
+    for (let user of this.innerModel.getAll()) {
+      html += `<tr>
 				<td>${user.username}</td>
 				<td>${user.name}</td>
 				<td>${user.password}</td>
 				<td>
-					<button id="${user.username}-editButton" class="editUser" >Editar</button>
+						<button id="${user.username}-editButton" class="editUser" >Editar</button>
 				</td>
 				<td>
-					<button id="${user.username}-deleteButton" class="deleteUser">Borrar</button>
+						<button id="${user.username}-deleteButton" class="deleteUser">Borrar</button>
 				</td>
-			<tr>`
-		}
+			<tr>`;
+    }
 
-		html +=
-			`</table>
+    html += `</table>
 		<br>
-		<button id="${this.id}btnNewUser" class="newUser">Nuevo Usuario</button>
-		<button id="${this.id}btnEditUser" class="editUser">Editar Usuario</button>
-		<button id="${this.id}btnDeleteUser" class="deleteUser">Borrar Usuario</button>`;
+		<button id="${this.id}btnNewUser" class= "newUser">Nuevo Usuario</button>
+		<button id="${this.id}btnEditUser" class= "editUser">Editar Usuario</button>
+    <button id="${this.id}btnDeleteUser" class= "deleteUser">Borrar Usuario</button>
+    </div>`;
 
-		document.getElementById(this.id).innerHTML = html;
+    document.getElementById(this.id).innerHTML = html;
 
+    document.getElementById(this.id + 'btnNewUser').addEventListener("click", (event) => this.onNewUserButtonClick(event));
+    document.getElementById(this.id + 'btnEditUser').addEventListener("click", (event) => this.onEditButtonClick(event));
+    document.getElementById(this.id + 'btnDeleteUser').addEventListener("click", (event) => this.onDeleteButtonClick(event));
 
-		document.getElementById(this.id + 'btnNewUser').addEventListener('click', event => this.onNewUserButtonClick(event));
-		document.getElementById(this.id + 'btnDeleteUser').addEventListener('click', event => this.onDeleteButtonClick(event));
-		document.getElementById(this.id + 'btnEditUser').addEventListener('click', event => this.onEditButtonClick(event));
+    // funciona pero con doble Click... Hay que descubrir por que.
+    document.getElementById("CRUD-Wrapper").addEventListener("click", (event) => {
 
-		// probar despues como arreglarlo
+      if (event.target.matches("button.editUser")) {
+        document.getElementById(event.target.id).addEventListener("click", (event) => this.onEditButtonClick(event));
+      } else if (event.target.matches("button.deleteUser")) {
+        document.getElementById(event.target.id).addEventListener("click", (event) => this.onDeleteButtonClick(event));
+      } else {
+        return
+      }
+    });
 
-		/* document.getElementById('CRUD-Tabble').addEventListener('click', event => { // Step 2
-			if (event.target.className === 'editUser') { // Step 3
-				document.getElementById(this.id).addEventListener('click', event => this.onEditButtonClick(event));
-			}
-		});
-		document.getElementById('CRUD-Tabble').addEventListener('click', event => { // Step 2
-			if (event.target.className === 'deleteUser') { // Step 3
-				document.getElementById(this.id).addEventListener('click', event => this.onDeleteButtonClick(event));
-			}
-		}); */
+  }
 
-	}
+  checkIfIsRight() {
+    if (this.innerModel.isUsernameAlreadyExists(id) === false) {
+      alert("Ese Nombre esta en uso");
+    }
+  }
 
-	checkIfIsRight() {
-		if (this.innerModel.isUsernameAlreadyExists(id) === false) {
-			alert("Ese Nombre esta en uso");
-		}
-	}
+  onNewUserButtonClick() {
 
-	onNewUserButtonClick() {
+    let userName = window.prompt("Ingrese Username para el nuevo usuario");
+    let name = window.prompt("Ingrese el Nombre del nuevo usuario");
+    let pass = window.prompt("Ingrese el Password del nuevo usuario");
 
-		let userName = window.prompt('Ingrese Username para el nuevo usuario');
-		let name = window.prompt('Ingrese el Nombre del nuevo usuario');
-		let pass = window.prompt('Ingrese el Password del nuevo usuario');
+    this.innerModel.create({
+      username: userName,
+      name: name,
+      password: pass,
+    });
+    this.update();
+  }
 
-		this.innerModel.create({
-			username: userName,
-			name: name,
-			password: pass
-		});
-		this.update();
-	}
+  onEditButtonClick() {
+    let userNameToEdit = window.prompt("Ingrese Username que desea Editar");
 
-	onEditButtonClick() {
+    let editedUserData = {
+      username: window.prompt("Ingrese Username para el nuevo usuario"),
+      name: window.prompt("Ingrese el Nombre del nuevo usuario"),
+      password: window.prompt("Ingrese el Password del nuevo usuario"),
+    };
 
-		let userNameToEdit = window.prompt('Ingrese Username que desea Editar');
+    this.innerModel.edit(userNameToEdit, editedUserData);
+    this.update();
+  }
 
-		let editedUserData = {
-			username: window.prompt('Ingrese Username para el nuevo usuario'),
-			name: window.prompt('Ingrese el Nombre del nuevo usuario'),
-			password: window.prompt('Ingrese el Password del nuevo usuario')
-		}
-
-		this.innerModel.edit(userNameToEdit, editedUserData);
-		this.update();
-	}
-
-	onDeleteButtonClick() {
-		let userName = window.prompt('Ingrese el Username que desea Borrar');
-		window.confirm('¿Está seguro de borrar el usuario?');
-		this.innerModel.delete(userName);
-		this.update();
-	}
-
+  onDeleteButtonClick() {
+    let userName = window.prompt("Ingrese el Username que desea Borrar");
+    window.confirm("¿Está seguro de borrar el usuario?");
+    this.innerModel.delete(userName);
+    this.update();
+  }
 }
 
-
-
 export {
-	UserView
+  UserView
 };
